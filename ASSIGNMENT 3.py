bcf9689fc290176df0c0d053bb2263e6c8b5ca97
anieysarh <145836687+anieysarh@users.noreply.github.com>
2023-09-23 12:11:03 +0100

    Add files via upload

---
+++ ASSIGNMENT 3.py
@@ -0,0 +1,31 @@
+#ASSIGNMENT 3
+
+numbers="0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90"
+print(numbers)
+
+for odd_num in range(0,90):
+    print(odd_num)
+    
+for i in range(0,99+1):
+    if i%2==1:
+        print(i)
+        
+length=input("enter someone fullname")
+if length<=10:
+    print("very short name")
+    
+if(lenght>10)and(length<=20):
+        print("normal name length")
+else:
+    print("long name")
+        
+    
+    
+
+    
+
+
+
+
+
+    
